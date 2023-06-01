cc680757c261249a674ecb2c641487169ded43af
water <water@staging.com>
2023-06-01 10:06:45 +0000

    feat: add config

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 require('./env')
+const config = require('config')
 const express = require('express');
 const app = express();
 
 app.get('/env', function (req, res) {
     console.log(process.env)
     res.send(process.env.NODE_ENV)
+})
+app.get('/config', function (req, res) {
+    res.send(config.get('name'))
 })
 app.get('/', function (req, res) {
     res.send('Hello')
